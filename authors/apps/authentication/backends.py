6d296980dc7ace65053212869a80d73b59e8427c
Mugerwa Fred <reifred33@gmail.com>
2019-03-28 10:29:22 +0300

    feat(jwt authentication): implement jwt authentication (#9)
    - ensure the app is nologer using session authentication
    - ensure the user recieves jwt token response on sign up and log in
    - refactor tests to use token based authentication
    - create a custom jwt backend
    - register custom jwt backend with djando rest framework
    
    [Finishes #164691214]

--- authors/apps/authentication/backends.py
+++ authors/apps/authentication/backends.py
@@ -1,10 +1,46 @@
-# import jwt
-#
-# from django.conf import settings
-#
-# from rest_framework import authentication, exceptions
-#
-# from .models import User
+import jwt
+from django.conf import settings
+from rest_framework import authentication, exceptions
+from .models import User
+from rest_framework.response import Response
 
 """Configure JWT Here"""
 
+
+class JWTAuthentication(authentication.BaseAuthentication):
+
+    def authenticate(self, request):
+
+        auth_header = authentication.get_authorization_header(request).split()
+
+        if not auth_header:
+            return None
+
+        token_type = auth_header[0].decode('utf-8')
+
+        if token_type not in ["Token", "Bearer"]:
+            msg = 'Bad Authorization header.'
+            raise exceptions.AuthenticationFailed(msg)
+
+        token = auth_header[1].decode('utf-8')
+
+        try:
+            payload = jwt.decode(token, settings.SECRET_KEY)
+        except jwt.ExpiredSignatureError:
+            msg = 'Your token expired. Log In again.'
+            raise exceptions.AuthenticationFailed(msg)
+        except jwt.InvalidTokenError:
+            msg = 'Invalid authentication. Could not decode token.'
+            raise exceptions.AuthenticationFailed(msg)
+
+        try:
+            user = User.objects.get(username=payload['usn'])
+        except User.DoesNotExist:
+            msg = f"user with username {payload['usn']} is not found"
+            raise exceptions.AuthenticationFailed(msg)
+
+        if not user.is_active:
+            msg = 'User nolonger has account with us'
+            raise exceptions.AuthenticationFailed(msg)
+
+        return (user, token)
