0093a4e925b590110c32e1091538b9910e9aa9b4
Anorld Mukone <manorldsapiens@gmail.com>
2019-04-10 10:19:48 +0300

    feat(user deletes article): loggedin user deletes their article - Only user created article can delete it [Starts #165177129]

--- authors/settings.py
+++ authors/settings.py
@@ -15,6 +15,13 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+
+from dotenv import load_dotenv
+project_folder = os.path.expanduser('~/ah-django-Aqua')  # adjust as appropriate
+load_dotenv(os.path.join(project_folder, '.env'))
+
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -156,10 +163,10 @@ NOSE_ARGS = [
         '--cover-erase',
 ]
 
-# Settings for configuring the app to Heroku
-if '/app' in os.environ['HOME']:
-    import django_heroku
-    django_heroku.settings(locals())
+# # Settings for configuring the app to Heroku
+# if '/app' in os.environ['HOME']:
+#     import django_heroku
+#     django_heroku.settings(locals())
 
 # Email settings
 EMAIL_BACKEND = os.environ.get('EMAIL_BACKEND', '')
