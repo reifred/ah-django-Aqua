92d3071f61e808b54f27243ba7e543504761875a
Anorld Mukone <manorldsapiens@gmail.com>
2019-04-15 09:09:55 +0300

    Remove unnecessary files for Travis

--- authors/settings.py
+++ authors/settings.py
@@ -15,13 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-from dotenv import load_dotenv
-project_folder = os.path.expanduser('~/ah-django-Aqua')  # adjust as appropriate
-load_dotenv(os.path.join(project_folder, '.env'))
-
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
