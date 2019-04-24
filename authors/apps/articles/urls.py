82cb48d9b3713b25cf04e9655952b2ecf0e4889c
NicholusMuwonge <43015966+NicholusMuwonge@users.noreply.github.com>
2019-04-24 15:32:18 +0300

    feat(article ratings): user is able to rate an artcle (#23)
    - user can only choose from 1 to 5
    - article ratings are bsed on average score from all ratings
    - rating by default is 0
    
    [Finishes #164691223]

--- authors/apps/articles/urls.py
+++ authors/apps/articles/urls.py
@@ -3,13 +3,15 @@ from django.urls import path
 
 from .views import (
     ListCreateArticleAPIView, RetrieveUpdateArticleAPIView, 
-    ArticleFavouriteAPIView
+    ArticleFavouriteAPIView,
+    RatingView
 )
 
 urlpatterns = [
     path('articles/', ListCreateArticleAPIView.as_view()),
     path('articles/<slug:slug>/', RetrieveUpdateArticleAPIView.as_view()),
     path('articles/', include(('authors.apps.comments.urls', 'comments'), namespace='comments')),
-    path('articles/<slug:slug>/favourite/', ArticleFavouriteAPIView.as_view())
+    path('articles/<slug:slug>/favourite/', ArticleFavouriteAPIView.as_view()),
+    path('articles/<str:slug>/rate/', RatingView.as_view()),
 ]
 
